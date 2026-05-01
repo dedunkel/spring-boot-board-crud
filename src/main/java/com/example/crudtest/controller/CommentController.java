a004449e16ad65b791fc2296d83a03fbca7700f6
dedunkel <skytop02@naver.com>
2026-05-01 23:31:24 +0900

    Add entity relationships (1:1, 1:N, M:N) with BoardDetail, Comment, Tag

--- src/main/java/com/example/crudtest/controller/CommentController.java
+++ src/main/java/com/example/crudtest/controller/CommentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/commnets")
+@RequestMapping("/api/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
