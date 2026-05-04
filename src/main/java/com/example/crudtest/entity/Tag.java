92c3c9193a0068c3f43b1dea0dec4af1582e4590
dedunkel <skytop02@naver.com>
2026-05-04 21:06:57 +0900

    Update some src files.

--- src/main/java/com/example/crudtest/entity/Tag.java
+++ src/main/java/com/example/crudtest/entity/Tag.java
@@ -17,6 +17,7 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "tag")
